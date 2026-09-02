05f393e524281fd84caa94e4ef766fb9c6a93df0
kameusagiyahoo <nywhatisbudou@yahoo.co.jp>
2026-09-02 15:05:27 +0900

    refactor: register all games from one module

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,18 +1,6 @@
-import {registerGame} from './core/registry.js';
-import {gridGame} from './games/grid.js';
-import {allocationGame} from './games/allocation.js';
-import {portfolioGame} from './games/portfolio.js';
-import {sequenceGame} from './games/sequence.js';
-import {frontlineGame} from './games/frontline.js';
-import {priorityGame} from './games/priority.js';
-import {isolationGame} from './games/isolation.js';
-import {gateGame} from './games/gate.js';
-import {triadGame} from './games/triad.js';
-import {memoryGame} from './games/memory.js';
-import {routeGame} from './games/route.js';
-import {patternGame} from './games/pattern.js';
+import {registerAllGames} from './games/index.js';
 
-[gridGame,allocationGame,portfolioGame,sequenceGame,frontlineGame,priorityGame,isolationGame,gateGame,triadGame,memoryGame,routeGame,patternGame].forEach(registerGame);
+registerAllGames();
 
 try{
   const settingsKey='partyPocketPartySettingsV1';
