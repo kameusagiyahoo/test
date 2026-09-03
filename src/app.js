f3224daaf853f505c62649ec68ddadfe04c360d1
kameusagiyahoo <nywhatisbudou@yahoo.co.jp>
2026-09-03 12:33:19 +0900

    chore: bump app version to 8.32.12

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import {createAppState} from './app/state.js';
 import {createAppRuntime} from './app/runtime.js';
 
-const APP_VERSION='8.32.11';
+const APP_VERSION='8.32.12';
 
 const runtime=createAppRuntime({
   state:createAppState(),
